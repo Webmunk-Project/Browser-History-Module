4478918e6f9fe799712681aab3354d118d4b3e5c
Chris Karr <github@audacious-software.com>
2024-09-11 18:37:42 -0500

    Module updates

--- js/worker.js
+++ js/worker.js
@@ -1,6 +1,8 @@
 /* global chrome, registerCustomModule, registerMessageHandler */
 
 (function () {
+  let moduleConfig = {}
+
   const fetchHistory = function (request, sender, sendResponse) {
     chrome.storage.local.get({ 'browser-history-last-fetch': 0 }, function (result) {
       let lastFetch = result['browser-history-last-fetch']
@@ -9,15 +11,22 @@
         lastFetch = request.fetchSince
       }
 
+      let sinceAgo = moduleConfig['browser-history-since-ago']
+
+      console.log(`[Browser History] Fetch up to ${sinceAgo} days ago. Last: ${lastFetch}`)
+      // console.log(result)
+
       const now = Date.now()
 
-      const monthAgo = now - (1000 * 60 * 60 * 24 * 30)
+      if (lastFetch === 0) {
+        if (sinceAgo === undefined || sinceAgo === null) {
+          sinceAgo = (1000 * 60 * 60 * 24 * 180) // 180 days
+        } else {
+          sinceAgo = (1000 * 60 * 60 * 24 * sinceAgo)
+        }
 
-      if (lastFetch < monthAgo) {
-        lastFetch = monthAgo
+        lastFetch = now - sinceAgo
       }
-
-      console.log(`[Browser History]: Fetch ${lastFetch}...`)
 
       chrome.storage.local.set({
         'browser-history-last-fetch': now
@@ -33,16 +42,24 @@
             chrome.history.getVisits({
               url: historyItem.url
             }, function (visits) {
-              visits.forEach(function (visit) {
-                const visitLogItem = {
-                  visit,
-                  url: historyItem.url,
-                  historyItem,
-                  id: visit.visitId,
-                  referrer_id: visit.referringVisitId
-                }
+              // TODO: Filter > lastFetch
 
-                results.push(visitLogItem)
+              visits.forEach(function (visit) {
+                if (visit.visitTime > lastFetch) {
+                  console.log(visit)
+
+                  const visitLogItem = {
+                    visit,
+                    url: historyItem.url,
+                    historyItem,
+                    id: visit.visitId,
+                    referrer_id: visit.referringVisitId
+                  }
+  
+                  results.push(visitLogItem)
+                } else {
+                  console.log('SKIP')
+                }
               })
 
               if (historyItem === historyItems[historyItems.length - 1]) {
@@ -60,12 +77,19 @@
   }
 
   registerCustomModule(function (config) {
-    if (config === null || config === undefined) {
-      config = {}
+    if (config === null) {
+      return
+    }
+    
+    moduleConfig = config['browser-history']
+
+    if (moduleConfig === null || moduleConfig === undefined) {
+      moduleConfig = {}
     }
 
     registerMessageHandler('fetch_browser_history', fetchHistory)
 
     console.log('[Browser History] Initialized.')
+    // console.log(config)
   })
 })()
